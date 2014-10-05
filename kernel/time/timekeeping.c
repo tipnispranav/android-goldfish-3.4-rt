2df02c26b71404454395fa595558e64ce6e890a5
Pranav Tipnis <pranav.tipnis@gmail.com>
2014-10-05 17:36:03 -0400

    Compilation error fix
    Signed-off-by: Pranav Tipnis <pranav.tipnis@gmail.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -1026,8 +1026,8 @@ static cycle_t logarithmic_accumulation(cycle_t offset, int shift)
 		leap = second_overflow(timekeeper.xtime.tv_sec);
 		timekeeper.xtime.tv_sec += leap;
 		timekeeper.wall_to_monotonic.tv_sec -= leap;
-		if (leap)
-			clock_was_set_delayed();
+		//if (leap)
+		//	clock_was_set_delayed();
 	}
 
 	/* Accumulate raw time */
@@ -1148,8 +1148,8 @@ static void update_wall_time(void)
 		leap = second_overflow(timekeeper.xtime.tv_sec);
 		timekeeper.xtime.tv_sec += leap;
 		timekeeper.wall_to_monotonic.tv_sec -= leap;
-		if (leap)
-			clock_was_set_delayed();
+		//if (leap)
+		//	clock_was_set_delayed();
 	}
 
 	timekeeping_update(false);
